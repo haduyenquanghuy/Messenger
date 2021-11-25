f56d8c67c4a62d94ea0e9c1dc6c99a32b6002880
Quang Huy Ha Duyen <haduyenquanghuy@gmail.com>
2021-11-25 20:37:46 +0700

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Ha Duyen Quang Huy on 25/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

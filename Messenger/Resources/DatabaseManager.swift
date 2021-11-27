c8eae5b60db4277f7d4f069dcdb36c97cedc0feb
Quang Huy Ha Duyen <haduyenquanghuy@gmail.com>
2021-11-27 21:17:53 +0700

    Rebase

--- Messenger/Resources/DatabaseManager.swift
+++ Messenger/Resources/DatabaseManager.swift
@@ -25,7 +25,10 @@ extension DatabaseManager{
         // Firebase database allows you to observe value changes on any entry in your no SQL DB
         // by specifying the child that you want to observe for
         // and specifying what type of observation you want
-        database.child(email).observeSingleEvent(of: .value, with: { snapshot in
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        
+        database.child(safeEmail).observeSingleEvent(of: .value, with: { snapshot in
             // this snapshot has a value property off of it that can be optional if doesn't exist
             // if has email -> call completion; pass value = true, else value = false;
             guard snapshot.value as? String != nil else {
@@ -39,7 +42,7 @@ extension DatabaseManager{
     /// Inserts new user to database
     public func insertUser(with user:ChatAppUser) {
         // emailAddress become primary key => can't have 2 user same email
-        database.child(user.emailAddress).setValue([
+        database.child(user.safeEmail).setValue([
             "first_name": user.firstName,
             "last_name": user.lastName
         ])
@@ -50,6 +53,13 @@ struct ChatAppUser {
     let firstName: String
     let lastName: String
     let emailAddress: String
+    
+    var safeEmail: String {
+        var safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        
+        return safeEmail
+    }
     //    let profilePictureURL: String
 }
 

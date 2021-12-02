8945b24f697b8b1cdd237d878424d1390a885d44
Quang Huy Ha Duyen <haduyenquanghuy@gmail.com>
2021-12-02 22:22:12 +0700

    Part 11: Sending Messages (Real-time)

--- Messenger/Resources/DatabaseManager.swift
+++ Messenger/Resources/DatabaseManager.swift
@@ -58,7 +58,7 @@ extension DatabaseManager{
                 return
             }
             
-             // multiple entry for each uses
+            // multiple entry for each uses
             
             self.database.child("users").observeSingleEvent(of: .value, with: { snapshot in
                 if var usersCollection = snapshot.value as? [[String: String]]{
@@ -110,6 +110,162 @@ extension DatabaseManager{
             completion(.success(value))
         })
     }
+}
+
+// MARK: - Sending messages / conversations
+extension DatabaseManager {
+    
+    /// Creates a new conversation with target user email and first message sent
+    public func createNewConversation(with otherUserEmail: String, firstMessage: Message, completion :@escaping(Bool) -> Void){
+        guard let currentEmail = UserDefaults.standard.value(forKey: "email") as? String else {
+            return
+        }
+        let safeEmail = DatabaseManager.safeEmail(emailAddress: currentEmail)
+        let ref = database.child("\(safeEmail)")
+        ref.observeSingleEvent(of: .value, with: {snapshot in
+            guard var userNode = snapshot.value as? [String:Any] else {
+                completion(false)
+                print("user not found")
+                return
+            }
+            
+            let messageDate = firstMessage.sentDate
+            let dateString = ChatViewController.dateFormatter.string(from: messageDate)
+            var message = ""
+            switch firstMessage.kind {
+            case .text(let messageText):
+                message = messageText
+            case .attributedText(_):
+                break
+            case .photo(_):
+                break
+            case .video(_):
+                break
+            case .location(_):
+                break
+            case .emoji(_):
+                break
+            case .audio(_):
+                break
+            case .contact(_):
+                break
+            case .linkPreview(_):
+                break
+            case .custom(_):
+                break
+            }
+            let conversationID = "conversation_\(firstMessage.messageId)"
+            
+            let newConversation:[String: Any] = [
+                "id": conversationID,
+                "other_user_email": otherUserEmail,
+                "lastest_message" :[
+                    "date": dateString,
+                    "message": message,
+                    "is_read": false
+                ]
+            ]
+            
+            if var conversations = userNode["conversations"] as? [[String: Any]] {
+                // conversation array exists for current user
+                // you should append
+                conversations.append(newConversation)
+                userNode["conversations"] = conversations
+            } else {
+                // conversation array does NOT exist
+                // create it
+                userNode["conversations"] = [
+                    newConversation
+                ]
+            }
+            
+            ref.setValue(userNode, withCompletionBlock: {error, _ in
+                guard error == nil else {
+                    completion(false)
+                    return
+                }
+                
+                self.finishCreatingConversation(conversationID: conversationID, firstMessage: firstMessage, completion: completion)
+            })
+        })
+    }
+    
+    private func finishCreatingConversation(conversationID: String, firstMessage: Message, completion: @escaping (Bool) -> Void) {
+        
+        var message = ""
+        let messageDate = firstMessage.sentDate
+        let dateString = ChatViewController.dateFormatter.string(from: messageDate)
+        switch firstMessage.kind {
+        case .text(let messageText):
+            message = messageText
+        case .attributedText(_):
+            break
+        case .photo(_):
+            break
+        case .video(_):
+            break
+        case .location(_):
+            break
+        case .emoji(_):
+            break
+        case .audio(_):
+            break
+        case .contact(_):
+            break
+        case .linkPreview(_):
+            break
+        case .custom(_):
+            break
+        }
+        
+        guard let myEmail = UserDefaults.standard.value(forKey: "email") as? String else {
+            completion(false)
+            return
+        }
+        
+        let currentUserEmail = DatabaseManager.safeEmail(emailAddress: myEmail)
+        
+        let collectionMessage: [String: Any] = [
+            "id": firstMessage.messageId,
+            "type": firstMessage.kind.messageKindString,
+            "content": message,
+            "date": dateString,
+            "sender_email": currentUserEmail,
+            "is_read": false
+        ]
+        
+        print("adding conversation: \(conversationID)")
+        
+        let value: [String: Any] = [
+            "messages": [
+                collectionMessage
+            ]
+        ]
+        
+        database.child("\(conversationID)").setValue(value, withCompletionBlock: {error,_ in
+            guard error == nil else {
+                completion(false)
+                return
+            }
+            completion(true)
+        })
+    }
+    
+    /// Fetch and returns all conversations for the user with passed in email
+    public func getAllConversations(for email:String, completion: @escaping (Result<String, Error>) -> Void){
+        
+    }
+    
+    /// Gets all messages for a given conversation
+    public func getAllMessagesForConversation(with id:String, completion: @escaping(Result<String, Error>)-> Void){
+        
+    }
+    
+    /// Sends a message with target conversation and message
+    public func sendMessage(to conversation: String, message:Message, completion: @escaping (Bool) -> Void){
+        
+    }
+    
 }
 
 public enum DatabaseError: Error {
